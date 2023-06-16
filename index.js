7b8542251fd45397765f68ba10d087fd57030891
MhdFarhan11 <117511556+MhdFarhan11@users.noreply.github.com>
2023-06-16 11:08:37 +0700

    Tambahkan File

---
+++ index.js
@@ -0,0 +1,48 @@
+/* MATERI IF ELSE */
+
+/* var mahasiswa1 = "budi"
+var mahasiswa2 = "joko"
+var kelas1 = "javascript"
+var kelas2 = "PHP"
+ if ( mahasiswa1 == "Javascript"){
+ console.log("yang kamu ambil berada di ruangan labor 103 kelas di mulai pada jam 10:00 am");
+}else if ( mahasiswa2 == "PHP"){
+console.log("yang kamu ambil berada di ruangan labor 101 kelas di mulai pada jam 01:00 pm pagi");
+} else {
+    console.log (" mohon maaf anda tidak mengambil kelas kafe koding");
+}  */
+
+/*  MATERI FUNCTION */
+
+/*  function warung (kopi, gula, minyak, beras)
+{
+    let HargaKopi = kopi * 5000
+    let HargaGula = gula * 4000
+    let HargaMinyak = minyak * 6000
+    let HargaBeras = beras * 12000
+    let total = HargaKopi + HargaGula + HargaMinyak + HargaBeras
+    return total 
+}
+console.log('Harga untuk membeli Beras 3 gantang adalah Rp.' + warung(0, 0, 0, 3)) */
+
+
+// MATERI ARRAY //
+
+const kafekoding = []
+    kafekoding[0] = 'php'
+    kafekoding[1] = 'javascript'
+    kafekoding[2] = 'designgrafis'
+    kafekoding[3] = 'ui/ux'
+    kafekoding[4] = 'python'
+const python = []
+    python [0] = 'budi'
+    python [1] = 'joko'
+    python [2] = 'anwar'
+kafekoding[5] = 'htmlandcss'
+kafekoding[6] = 'kotlin'
+const kotlin = []
+    kotlin [0] = 'adit'
+    kotlin [1] = 'mawar'
+    kotlin [2] = 'dona'
+
+console.log('mahasiswa ' +kafekoding[6]+ ' yang bernama ' +kotlin[2]+ ' sedang sakit ')
